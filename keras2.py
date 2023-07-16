be18e4d4c436b6a3610e2336f50c61993022c516
Error <shkum79@ukr.net>
2023-07-16 11:12:10 +0200

    Initial commit

---
+++ keras2.py
@@ -0,0 +1,9 @@
+import keras
+import tensorflow as tf
+
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)),
+    keras.layers.Dense(128, activation=tf.nn.relu),
+    keras.layers.Dense(10, activation=tf.nn.softmax)
+])
+
