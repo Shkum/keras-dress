ca5eec3851001c2a64152041ccad02b2927725b7
Sergiy <37875819+Shkum@users.noreply.github.com>
2023-07-20 10:13:11 +0200

    Update

--- c1_week3.py
+++ c1_week3.py
@@ -1,5 +1,7 @@
 import tensorflow as tf
 
+import matplotlib.pyplot as plt
+
 print(tf.__version__)
 
 # Load the Fashion MNIST dataset
@@ -17,11 +19,10 @@ test_images = test_images / 255.0
 
 # Build the classification model
 model = tf.keras.models.Sequential([
-    tf.keras.layers.Conv2D(64, (3,3), activation='relu', input_shape=(28, 28,1)),
-                                    tf.keras.layers.Flatten(),
-                                    tf.keras.layers.Dense(128, activation=tf.nn.relu),
-                                    tf.keras.layers.Dense(10, activation=tf.nn.softmax)])
-
+    tf.keras.layers.Conv2D(64, (3, 3), activation='relu', input_shape=(28, 28, 1)),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(128, activation=tf.nn.relu),
+    tf.keras.layers.Dense(10, activation=tf.nn.softmax)])
 
 model.compile(optimizer=tf.optimizers.Adam(),
               loss='sparse_categorical_crossentropy',
@@ -40,33 +41,28 @@ print(result_list)
 result = max(list(enumerate(result_list)), key=lambda x: x[1])
 print(f'{result[1]}% ---> {class_names[result[0]]}')
 
-
-
 lab = test_labels[:100]
 print(list(enumerate(lab)), sep='\n')
 
-import matplotlib.pyplot as plt
-from keras import models
+f, axarr = plt.subplots(3, 4)
 
-f, axarr = plt.subplots(3,4)
-
-FIRST_IMAGE=0
-SECOND_IMAGE=23
-THIRD_IMAGE=28
+FIRST_IMAGE = 0
+SECOND_IMAGE = 23
+THIRD_IMAGE = 28
 CONVOLUTION_NUMBER = 2
 
 layer_outputs = [layer.output for layer in model.layers]
-activation_model = tf.keras.models.Model(inputs = model.input, outputs = layer_outputs)
+activation_model = tf.keras.models.Model(inputs=model.input, outputs=layer_outputs)
 
-for x in range(0,4):
-  f1 = activation_model.predict(test_images[FIRST_IMAGE].reshape(1, 28, 28, 1))[x]
-  axarr[0,x].imshow(f1[0, : , :, CONVOLUTION_NUMBER], cmap='inferno')
-  axarr[0,x].grid(False)
+for x in range(0, 4):
+    f1 = activation_model.predict(test_images[FIRST_IMAGE].reshape(1, 28, 28, 1))[x]
+    axarr[0, x].imshow(f1[0, :, :, CONVOLUTION_NUMBER], cmap='inferno')
+    axarr[0, x].grid(False)
 
-  f2 = activation_model.predict(test_images[SECOND_IMAGE].reshape(1, 28, 28, 1))[x]
-  axarr[1,x].imshow(f2[0, : , :, CONVOLUTION_NUMBER], cmap='inferno')
-  axarr[1,x].grid(False)
+    f2 = activation_model.predict(test_images[SECOND_IMAGE].reshape(1, 28, 28, 1))[x]
+    axarr[1, x].imshow(f2[0, :, :, CONVOLUTION_NUMBER], cmap='inferno')
+    axarr[1, x].grid(False)
 
-  f3 = activation_model.predict(test_images[THIRD_IMAGE].reshape(1, 28, 28, 1))[x]
-  axarr[2,x].imshow(f3[0, : , :, CONVOLUTION_NUMBER], cmap='inferno')
-  axarr[2,x].grid(False)
+    f3 = activation_model.predict(test_images[THIRD_IMAGE].reshape(1, 28, 28, 1))[x]
+    axarr[2, x].imshow(f3[0, :, :, CONVOLUTION_NUMBER], cmap='inferno')
+    axarr[2, x].grid(False)
